329bd007974a2f8c5079190e929796ce8cbb1007
Akshaykamble1 <95301177+Akshaykamble1@users.noreply.github.com>
2023-11-09 10:35:42 +0530

    Add files via upload

---
+++ vaiableTypes.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class vaiableTypes 
+{
+	int num = 23;//instance variable
+	
+	void show()
+	{
+		int num2 = 34;//local variable
+		System.out.println("Number is : "+num2);
+		System.out.println("Number is : "+num);
+	}
+	
+	public static void main(String[] args) 
+	{
+		vaiableTypes obj = new vaiableTypes();
+		obj.show();
+		System.out.println("Number is : "+obj.num);
+	}
+}
